7bae6c1833527e7041fb6c2273ba2a23c284fbd2
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-01 11:54:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php 
-    $servername = "ec2-18-204-74-74.compute-1.amazonaws.com";
-    $username = "rrazuzxslncjlg";
-    $password = "bb9362cb79376fa38b5d90c020c36dfd99c0184b4eb82f4405353190588f52b9";
-    $database = "ddcul1krvca7nt";
-
     $conn = pg_connect("host=ec2-18-204-74-74.compute-1.amazonaws.com dbname=ddcul1krvca7nt user=rrazuzxslncjlg password=bb9362cb79376fa38b5d90c020c36dfd99c0184b4eb82f4405353190588f52b9 port=5432");
 
     if(!conn){
@@ -12,8 +7,29 @@
     else{
         echo("Kết nối thành công");
     }
-?>
 
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql="SELECT * FROM account WHERE user_name ='$username' and pass='$password'";
+    $result = pg_query($conn, $sql);
+    if (!$result) {
+      echo "An error occurred.\n";
+      exit;
+    }
+
+    while ($row = pg_fetch_row($result)) {
+      echo "name: $row[1]  pass: $row[2]";
+      echo "<br />\n";
+    }
+        header('Location:chucmung.php');
+
+
+      }else{
+         echo '\n login status : false';
+     }
+?>
+<title>Demo</title>
 <form action="index.php" method="post">
   <div class="container">
     <label for="username"><b>Username</b></label>
@@ -33,19 +49,3 @@
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
-
-<?php
-  $taikhoan = $_POST['username'];
-  $password = $_POST['password'];
-
-  $sql = "SELECT * FROM account where username = '$taikhoan' and password ='$password'";
-
-  $rs = mysqli_query($conn, $sql);
-
-  if (mysqli_num_rows($rs) > 0){
-    echo "Dang nhap thanh cong";
-  }
-  else{
-    echo "Dang nhap that bai";
-  }
-?>
